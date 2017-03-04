62c3ef912a7d9f847111306bc71b43a84c1fcd58
Alex Corvi <alex@arrayy.com>
2017-03-04 16:10:31 +0300

    add a function not a result

--- src/index.ts
+++ src/index.ts
@@ -8,48 +8,48 @@ export namespace Fin {
 }
 
 export function sentiment(input:Fin.FinReturn){
-
-    // initialize all with 0
-    const sentiment:number[][] = [];
-    input.tokens.forEach((sentenceTokens,sentenceIndex)=>{
-        sentiment[sentenceIndex] = []; 
-        sentenceTokens.forEach((token,tokenIndex)=>{
-            sentiment[sentenceIndex][tokenIndex] = 0;
+    const calculateSentiment = function () {
+        // initialize all with 0
+        const sentiment:number[][] = [];
+        input.tokens.forEach((sentenceTokens,sentenceIndex)=>{
+            sentiment[sentenceIndex] = []; 
+            sentenceTokens.forEach((token,tokenIndex)=>{
+                sentiment[sentenceIndex][tokenIndex] = 0;
+            });
         });
-    });
+        // analyze and add scores
+        input.tokens.forEach((sentenceTokens,sentenceIndex)=>{
+            sentenceTokens.forEach((token,tokenIndex)=>{
+                const sentimentNum = dict[token.toLowerCase()];
+                if(sentimentNum) {
+                    const type = input.tags[sentenceIndex][tokenIndex].charAt(0).toUpperCase();
 
-    // analyze and add scores
-    input.tokens.forEach((sentenceTokens,sentenceIndex)=>{
-        sentenceTokens.forEach((token,tokenIndex)=>{
-            const sentimentNum = dict[token];
-            if(sentimentNum) {
-                const type = input.tags[sentenceIndex][tokenIndex].charAt(0).toUpperCase();
+                    // Verbs: apply to subjects and objects
+                    if (type === "V") {
+                        const dependents:number[] = [];
+                        input.deps[sentenceIndex].forEach((node,index)=>{
+                            if(node.parent === tokenIndex && node.label.toUpperCase().endsWith("BJ")) dependents.push(index);
+                        });
+                        dependents.forEach((index)=>{
+                            sentiment[sentenceIndex][index] += sentimentNum;
+                        });
+                    }
 
-                // Verbs: apply to subjects and objects
-                if (type === "V") {
-                    const dependents:number[] = [];
-                    input.deps[sentenceIndex].forEach((node,index)=>{
-                        if(node.parent === tokenIndex && node.label.toUpperCase().endsWith("BJ")) dependents.push(index);
-                    });
-                    dependents.forEach((index)=>{
-                        sentiment[sentenceIndex][index] += sentimentNum;
-                    });
+                    // interjections: apply to the sentence root
+                    else if(type === "U"){
+                        const rootIndex = input.deps[sentenceIndex].findIndex(depsNode=>depsNode.parent === -1);
+                        sentiment[sentenceIndex][rootIndex] += sentimentNum;
+                    }
+
+                    // all other: apply to parent
+                    else {
+                        const parentIndex = input.deps[sentenceIndex][tokenIndex].parent;
+                        sentiment[sentenceIndex][parentIndex] += sentimentNum;
+                    }
                 }
-
-                // interjections: apply to the sentence root
-                else if(type === "U"){
-                    const rootIndex = input.deps[sentenceIndex].findIndex(depsNode=>depsNode.parent === -1);
-                    sentiment[sentenceIndex][rootIndex] += sentimentNum;
-                }
-
-                // all other: apply to parent
-                else {
-                    const parentIndex = input.deps[sentenceIndex][tokenIndex].parent;
-                    sentiment[sentenceIndex][parentIndex] += sentimentNum;
-                }
-            }
+            });
         });
-    });
-
-    return Object.assign(input,{sentiment});
+        return sentiment;
+    };
+    return Object.assign(input,{sentiment:calculateSentiment});
 }
